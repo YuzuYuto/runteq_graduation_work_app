7198f6316273aad9cc4f8ff21f5844decb8addb9
YuzuYuto <yuutoyunoki@icloud.com>
2025-12-09 01:44:27 +0900

    投稿詳細・投稿編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show destroy edit update]
+  before_action :authorize_user, only: %i[edit update destroy]
+
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
   end
@@ -18,19 +21,39 @@ class PostsController < ApplicationController
     end
   end
 
+  def show 
+  end
+
   def destroy 
-    @post = Post.find(params[:id])
-    if @post.user == current_user 
-      @post.destroy 
-      redirect_to posts_path, notice: '投稿が削除されました'
+    @post.destroy 
+    redirect_to posts_path, notice: '投稿が削除されました'
+  end
+
+  def edit 
+  end
+
+  def update 
+    if @post.update(post_params)
+      redirect_to @post, notice: '投稿を更新しました'
     else
-      redirect_to posts_path, alert: '権限がありません'
+      flash.now[:alert] = '投稿の更新に失敗しました'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
+  def set_post 
+    @post = Post.find(params[:id])
+  end
+
   def post_params 
     params.require(:post).permit(:title, :body)
+  end
+
+  def authorize_user 
+    unless @post.user == current_user 
+      redirect_to posts_path, alert: '権限がありません'
+    end
   end
 end
